00eed440acbb29b94e5c2fadfa6d763c5b61a819
John Doe <johndoe@email.com>
2025-08-07 23:42:19 +0500

    Sample

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './Header.css';
-import PopUser from '../Popups/PopUser/PopUser';
+import "./Header.css";
+import PopUser from "../Popups/PopUser/PopUser";
+import { A, Auser, DivBlock, DivContainer, DivLogo, Nav, SButton, SHeader } from "./Header.styled";
 
 export default function Header() {
   const [isUserPopupOpen, setIsUserPopupOpen] = useState(false);
 
   return (
-    <header className="header">
-      <div className="container">
-        <div className="header__block">
-          <div className="header__logo _show _light">
-            <a href="/"><img src="/images/logo.png" alt="Логотип" /></a>
-          </div>
-          <nav className="header__nav">
-            <button className="header__btn-main-new _hover01">
-              <a href="#popNewCard">Создать новую задачу</a>
-            </button>
-            <a 
-              href="#user-set-target" 
+    <SHeader>
+      <DivContainer>
+        <DivBlock>
+          <DivLogo className="_show _light">
+            <a href="/">
+              <img src="/images/logo.png" alt="Логотип" />
+            </a>
+          </DivLogo>
+          <Nav className="header__nav">
+            <SButton className="_hover01">
+              <A href="#popNewCard">Создать новую задачу</A>
+            </SButton>
+            <a
               className="header__user _hover02"
               onClick={() => setIsUserPopupOpen(!isUserPopupOpen)}
             >
               Ivan Ivanov
             </a>
-            <PopUser 
-              isOpen={isUserPopupOpen} 
-              onClose={() => setIsUserPopupOpen(false)} 
+            <PopUser
+              isOpen={isUserPopupOpen}
+              onClose={() => setIsUserPopupOpen(false)}
             />
-          </nav>
-        </div>
-      </div>
-    </header>
+          </Nav>
+        </DivBlock>
+      </DivContainer>
+    </SHeader>
   );
 }
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+export const SHeader = styled.header`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+
+export const DivContainer = styled.div`
+ max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
+
+export const DivBlock = styled.div`
+ height: 70px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
+  padding: 0 10px;
+`;
+
+export const DivLogo = styled.div`
+img {
+    width: 85px;
+  }
+`;
+
+export const Nav = styled.nav`
+max-width: 290px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SButton = styled.button`
+ width: 178px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: #565eef;
+  color: #ffffff;
+  border: none;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
+`;
+
+export const A = styled.a`
+color: #ffffff;
+`;
+
+export const Auser = styled.a`
+height: 20px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 20px;
+  color: #565eef;
+  box-sizing: border-box;
+`;
+
+
+
+export const HeaderBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+
+
+export const HeaderNav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
