286ae872594fa39f1ef87b5f2a45034bc9fe0e15
John Doe <johndoe@email.com>
2025-08-17 21:13:27 +0500

    Loader

--- src/components/CardLoader/Loader.jsx
+++ src/components/CardLoader/Loader.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Loader.css';
+import { DivOverlay, DivSpinner } from './Loader';
 
 export default function Loader() {
   return (
-    <div className="loader-overlay">
-      <div className="loader-spinner"></div>
-    </div>
+    <DivOverlay>
+      <DivSpinner></DivSpinner>
+    </DivOverlay>
   );
 }
